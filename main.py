04095762e85b23b7491fbe86986e4ef6af3087d7
Rin <62606765+chuangyu-hscy@users.noreply.github.com>
2023-03-23 01:02:45 +1100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ Author: Wei Zhao & Sunchuangyu Huang
 Github: https://github.com/rNLKJA/2023-S1-COMP90024-A1/
 
 """
+
+# local testing purpose - remove before release
+import os
+os.environ['NUMEXPR_MAX_THREADS'] = '32'
+
 from mpi4py import MPI
 import time
 
@@ -16,6 +21,11 @@ from scripts.logger import twitter_logger as logger
 from scripts.sal_processor import *
 from scripts.utils import obtain_args, split_file_into_chunks
 from scripts.twitter_processor import twitter_processor
+
+
+logger.info("PROGRAM START")
+
+
 
 PATH = Path()
 
@@ -27,7 +37,6 @@ sal_df = load_sal_parquet(PATH, logger)
 comm = MPI.COMM_WORLD
 rank, size = comm.Get_rank(), comm.Get_size()
 
-
 start_time = time.time()
 
 if __name__ == '__main__':
@@ -37,9 +46,11 @@ if __name__ == '__main__':
     tweet_df = twitter_processor(
         twitter_file, chunk_start[rank], chunk_end[rank])
 
+    logger.info("File read complete")
     comm.Barrier()
 
     if rank == 0:
+        logger.info("Retrive dataframe list")
         tweet_dfs = [tweet_df]
         for np in range(1, size):
             tweet_dfs.append(comm.recv(source=np))
@@ -47,12 +58,17 @@ if __name__ == '__main__':
         comm.send(tweet_df, dest=0)
 
     if rank == 0:
+        logger.info("Start merge dataframes")
         tdf = pd.concat(tweet_dfs)
         tdf.to_parquet("./data/processed/tmp.parquet")
+        
+        logger.info(f"Final dataframe has shape: {tdf.shape}")
 
     comm.Barrier()
 
-    logger.info(f"Programming running seconds: {time.time() - start_time}")
-
-    # end program after job complete
+    if rank == 0:
+        logger.info(f"Programming running seconds: {time.time() - start_time}")
+        # end program after job complete
+        logger.info("PROGRAM END")
+        
     exit()
