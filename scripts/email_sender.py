5412b647b743f2d67e8a3fd223c10f836fb56191
weizhao1 <zw0432751551@gmail.com>
2023-03-29 00:23:58 +1100

    add comments for email_sender.py

--- scripts/email_sender.py
+++ scripts/email_sender.py
@@ -1,12 +1,16 @@
+"""
+Log file sender through smtp server
+"""
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
-from .logger import FILENAME
-from .logger import FULL_PATH # import logger
+from .logger import FULL_PATH  # import log file path
 
-
+# send log file to developer's email for status check & running time collection
 def send_log():
+
+    # define smtp server and port
     smtp_server = 'smtp.163.com'
     smtp_port = 25
     smtp_username = 'zw0432751551@163.com'
@@ -14,17 +18,22 @@ def send_log():
     smtp_connection = smtplib.SMTP(smtp_server, smtp_port)
     smtp_connection.login(smtp_username, smtp_password)
 
+
+    # get the full path of log file
     log_file_path = FULL_PATH
-    
-    with open(log_file_path, 'rb') as file:
+
+    with open(log_file_path, 'r') as file:
         log_file_data = file.read()
 
+    # set the email content body
     message = MIMEText(log_file_data)
 
+
+    # set the email header
     message['From'] = 'zw0432751551@163.com'
     message['To'] = 'zw0432751551@gmail.com'
     message['Subject'] = 'Log file'
 
+    # send the email
     smtp_connection.sendmail(smtp_username, 'zw0432751551@gmail.com', message.as_string())
     smtp_connection.quit()
-    print ("Email sent successfully")
