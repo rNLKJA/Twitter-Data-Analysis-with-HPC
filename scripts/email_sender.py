5db940a6033f6d0ea299116b6c604d38057223f4
weizhao1 <zw0432751551@gmail.com>
2023-03-29 00:04:26 +1100

    send the log text in email body

--- scripts/email_sender.py
+++ scripts/email_sender.py
@@ -15,18 +15,15 @@ def send_log():
     smtp_connection.login(smtp_username, smtp_password)
 
     log_file_path = FULL_PATH
-    log_file_name = FILENAME
+    
     with open(log_file_path, 'rb') as file:
         log_file_data = file.read()
-    log_file_attachment = MIMEApplication(log_file_data, Name=log_file_name)
-    log_file_attachment['Content-Disposition'] = f'attachment; filename="{log_file_name}"'
-    message = MIMEMultipart()
-    message.attach(log_file_attachment)
+
+    message = MIMEText(log_file_data)
 
     message['From'] = 'zw0432751551@163.com'
     message['To'] = 'zw0432751551@gmail.com'
     message['Subject'] = 'Log file'
-    message.attach(MIMEText('Please find attached the log file.'))
 
     smtp_connection.sendmail(smtp_username, 'zw0432751551@gmail.com', message.as_string())
     smtp_connection.quit()
