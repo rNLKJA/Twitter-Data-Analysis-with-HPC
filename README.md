bbf59a9db074e12a9de6d25d221dfbe1bfe42f7b
rNLKJA <rNLKJA@non-use.com>
2023-04-01 17:07:52 +1100

    updated task1 and task 2 function

--- README.md
+++ README.md
@@ -46,53 +46,49 @@ Main Python dependencies: python=3.7.4, mpi4py=3.0.4, numpy, pandas.
 
 **Task 1 Question**: The solution should count the number of tweets made by the same individual based on the bigTwitter.json file and returned the top 10 tweeters in terms of the number of tweets made irrespective of where they tweeted. The result will be of the form (where the author Ids and tweet numbers are representative).
 
-| Rank | Author Id | Number of Tweets Made |
-|----|----|----|
-| 1	| 1498063511204760000|  	68477| 
-| 2	| 1089023364973210000|  	28128| 
-| 3	| 826332877457481000 | 	27718| 
-| 4	| 1250331934242120000 | 	25350| 
-| 5	| 1423662808311280000 | 	21034| 
-| 6	| 1183144981252280000 | 	20765| 
-| 7	| 1270672820792500000 | 	20503| 
-| 8	| 820431428835885000 | 	20063| 
-| 9	| 778785859030003000 | 	19403| 
-| 10| 	1104295492433760000 | 	18781| 
+| Rank | Author Id           | Number of Tweets Made |
+| ---- | ------------------- | --------------------- |
+| 1    | 1498063511204760000 | 68477                 |
+| 2    | 1089023364973210000 | 28128                 |
+| 3    | 826332877457481000  | 27718                 |
+| 4    | 1250331934242120000 | 25350                 |
+| 5    | 1423662808311280000 | 21034                 |
+| 6    | 1183144981252280000 | 20765                 |
+| 7    | 1270672820792500000 | 20503                 |
+| 8    | 820431428835885000  | 20063                 |
+| 9    | 778785859030003000  | 19403                 |
+| 10   | 1104295492433760000 | 18781                 |
 
+**Task 2 Question**: Using the _bigTwitter.json_ and _sal.json_ file you will then count the number of tweets made in the various captical cities by all users. The result will be a table of the form (where the numbers are representative).
 
-
-**Task 2 Question**: Using the *bigTwitter.json* and *sal.json* file you will then count the number of tweets made in the various captical cities by all users. The result will be a table of the form (where the numbers are representative). 
-
-For this task, ignore tweets made by users in rural location, e.g. *lrnsw* (Rural New South Wales), *1rvic* (Rural Victoria) etc.
-
+For this task, ignore tweets made by users in rural location, e.g. _lrnsw_ (Rural New South Wales), _1rvic_ (Rural Victoria) etc.
 
 | Greater Capital City | Number of Tweets Made |
-| :----: | :----: |
-| 1gsyd | 2122 |
-|2gmel|14665|
-|3gbri|1746|
-|4gade|11426|
-|5gper|578882|
-|6ghob|695|
-|7gdar|36|
-|8acte|193835|
-|9oter|3|
+| :------------------: | :-------------------: |
+|        1gsyd         |        2114445        |
+|        2gmel         |        2280650        |
+|        3gbri         |        855383         |
+|        4gade         |        473982         |
+|        5gper         |        589204         |
+|        6ghob         |         90917         |
+|        7gdar         |         46376         |
+|        8acte         |        195459         |
+|        9oter         |          203          |
 
 **Task 3 Question**: The solution should identify those tweeters that have a tweeted in the most Greater Capital cities and the number of times they have tweeted from those locations. The top 10 tweeters making tweets from the most different locations should be returned and if there are equal number of locations, then these should be ranked by the number of tweets. Only those tweets made in Greater Capital cities should be counted.
 
-|Rank|Author Id|Number of Unique City Locations and #Tweets|
-| :----: | :---- | :---- |
-|1|702290904460169216|8 (#206 - #5gsyd ,#12gmel ,#7gbri ,#8gade ,#127gper ,#2ghob ,#1gdar ,#44acte)|
-|2|87188071|5 (#82 - #1gsyd ,#2gmel ,#1gbri ,#45gper ,#33acte)|
-|3|61220731|4 (#646 - #109gmel ,#39gbri ,#29gade ,#469acte)"|
-|4|921197448885886977|4 (#39 - #2gmel ,#4gade ,#27gper ,#6acte)|
-|5|228664718|4 (#36 - #4gsyd ,#1gbri ,#27gper ,#4acte)|
-|6|2225880756|4 (#13 - #4gsyd ,#5gdar ,#3acte ,#1oter)|
-|7|1482557020528332801|4 (#6 - #1gsyd ,#2gbri ,#2gper ,#1acte)|
-|8|987122373395595264|4 (#5 - #1gsyd ,#1gbri ,#1gade ,#2acte)|
-|9|355186886|3 (#404 - #358gmel ,#1gper ,#45acte)|
-|10|565012072|3 (#111 - #1gsyd ,#1gper ,#109acte)|
-
+| Rank | Author Id           | Number of Unique City Locations and #Tweets                                   |
+| :--: | :------------------ | :---------------------------------------------------------------------------- |
+|  1   | 702290904460169216  | 8 (#206 - #5gsyd ,#12gmel ,#7gbri ,#8gade ,#127gper ,#2ghob ,#1gdar ,#44acte) |
+|  2   | 87188071            | 5 (#82 - #1gsyd ,#2gmel ,#1gbri ,#45gper ,#33acte)                            |
+|  3   | 61220731            | 4 (#646 - #109gmel ,#39gbri ,#29gade ,#469acte)"                              |
+|  4   | 921197448885886977  | 4 (#39 - #2gmel ,#4gade ,#27gper ,#6acte)                                     |
+|  5   | 228664718           | 4 (#36 - #4gsyd ,#1gbri ,#27gper ,#4acte)                                     |
+|  6   | 2225880756          | 4 (#13 - #4gsyd ,#5gdar ,#3acte ,#1oter)                                      |
+|  7   | 1482557020528332801 | 4 (#6 - #1gsyd ,#2gbri ,#2gper ,#1acte)                                       |
+|  8   | 987122373395595264  | 4 (#5 - #1gsyd ,#1gbri ,#1gade ,#2acte)                                       |
+|  9   | 355186886           | 3 (#404 - #358gmel ,#1gper ,#45acte)                                          |
+|  10  | 565012072           | 3 (#111 - #1gsyd ,#1gper ,#109acte)                                           |
 
 For complete assignment 1 report, please check [overleaf](https://www.overleaf.com/read/sdsczmmdxzvq).
 
--- scripts/twitter_processor.py
+++ scripts/twitter_processor.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from dataclasses import dataclass
 import re
 from .utils import normalise_location, is_state_location
+import polars as pl
+from scripts.logger import twitter_logger as logger
 
 
 @dataclass
@@ -31,7 +33,7 @@ def file_break_check(cb: int, ce: int) -> bool:
     ...
 
 
-def twitter_processor(filename: Path, cs: int, ce: int) -> pd.DataFrame:
+def twitter_processor(filename: Path, cs: int, ce: int, sal_df: pl.DataFrame) -> pl.DataFrame:
     """
     Processing twitter data from line by line and return a pandas dataframe
 
@@ -51,7 +53,7 @@ def twitter_processor(filename: Path, cs: int, ce: int) -> pd.DataFrame:
     SKIP_LINES_2 = 20
 
     # define results list
-    tweet_lst = []
+    tweets_id, author_id, location = [], [], []
 
     with open(filename, mode="rb") as f:
         # seek the start of the file
@@ -64,57 +66,159 @@ def twitter_processor(filename: Path, cs: int, ce: int) -> pd.DataFrame:
             # find target twitter id
             match_id = re.search(TWEETS_ID, line)
             if match_id:
-                _id = match_id.group(1)
-                tweet_lst.append(Twitter(_id=_id))
+                tweets_id.append(match_id.group(1))
 
                 next(f, None)
                 next(f, None)
 
             # find target author id
             match_author = re.search(AUTHOR_ID, line)
-            if match_author and tweet_lst:
-                author = match_author.group(1)
-                tweet_lst[-1].author = author
+            if match_author and len(tweets_id) != len(author_id):
+                author_id.append(np.int64(match_author.group(1)))
 
-                for _ in range(SKIP_LINES_1):  # skip 20 lines
+                for _ in range(SKIP_LINES_1):  # skip irrelevant lines
                     next(f, None)
 
             # find target location name
             match_location = re.search(LOCATION_ID, line)
-            if match_location and tweet_lst:
-                location = match_location.group(1)
-                tweet_lst[-1].location = location
+            if match_location and len(tweets_id) != len(location):
+                location.append(match_location.group(1))
 
-                for _ in range(SKIP_LINES_2):  # skip 20 lines
+                for _ in range(SKIP_LINES_2):  # skip irrelevant lines
                     next(f, None)
 
             # break condition check
             if f.tell() >= ce:
-                if tweet_lst[-1].location is None:
+                if len(tweets_id) != len(location):
                     continue
                 else:
                     EOF = True
 
     # convert result in a dataframe
-    tweet_df = pd.DataFrame([tweet.__dict__ for tweet in tweet_lst])
-    tweet_df.location = tweet_df.location.apply(
-        lambda x: normalise_location(x))
-    # tweet_df = tweet_df[~tweet_df.location.apply(lambda x: is_state_location(x))]
+    tdf = generate_polars_dataframe(tweets_id=tweets_id, author_id=author_id, location=location, sal_df=sal_df)
 
-    return tweet_df
+    logger.info("File read complete")
 
+    return tdf
 
-def task1(tweet_df: pd.DataFrame) -> pd.DataFrame:
-    rdf = tweet_df[['gcc', '_id']].groupby('gcc').count().reset_index()
-    return rdf
+def generate_polars_dataframe(tweets_id: list, author_id: list, location: list, sal_df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Generate a polars dataframe from a list of twitter objects
 
+    Args:
+        tweets_id (list): a list of twitter id
+        author_id (list): a list of author id
+        location (list): a list of location string
 
-def task2(tweet_df: pd.DataFrame) -> pd.DataFrame:
-    rdf = tweet_df[['author', '_id']].groupby('author').count().reset_index()
-    return rdf
+    Returns:
+        pl.DataFrame: a polars dataframe contains twitter_id, author_id, location
+    """
+    # convert result in a dataframe
+    tweet_df = pl.DataFrame({'tweet_id': tweets_id, 'author_id': author_id, 'location': location})
+    
+    tweet_df1 = tweet_df.with_columns(pl.col('location').apply(lambda x: normalise_location(x), skip_nulls=True))
+    tweet_df1 = tweet_df1.join(sal_df, on='location', how='left')
+    
+    tweet_df2 = tweet_df1.filter(pl.col('gcc').is_null())
+    if not tweet_df2.is_empty():
+        tweet_df2 = tweet_df2.with_columns(pl.col('location').apply(lambda x: x.split(' ')[0]))
 
+        tweet_df2 = tweet_df2[['tweet_id', 'author_id', 'location']]\
+                    .join(sal_df.with_columns(pl.col('location')\
+                    .cast(pl.Utf8)), on='location', how='left')
+        
+    tdf = tweet_df1.join(tweet_df2, on='tweet_id', how='left')
+    
+    tdf = tdf.with_columns(pl.col('gcc').fill_null(pl.col('gcc_right')))[['tweet_id', 'author_id', 'location', 'gcc']]
 
-def task3(tweet_df: pd.DataFrame) -> pd.DataFrame:
-    rdf = tweet_df[['author', 'gcc']].groupby(
-        ['author']).nunique().reset_index()
-    return rdf
+    tweet_df3 = tdf.filter(pl.col('gcc').is_null())
+    if not tweet_df3.is_empty():
+        
+        tweet_df3 = tweet_df3.with_columns(pl.col('location').apply(lambda x: ' '.join(x.split(' ')[0:2])))
+        tweet_df3 = tweet_df3[['tweet_id', 'author_id', 'location']]\
+                    .join(sal_df.with_columns(pl.col('location')\
+                    .cast(pl.Utf8)), on='location', how='left')
+    
+    tdf = tdf.join(tweet_df3, on='tweet_id', how='left')
+    tdf = tdf.with_columns(pl.col('gcc').fill_null(pl.col('gcc_right')))[['tweet_id', 'author_id', 'location', 'gcc']]
+
+    return tdf
+
+def combine_tdf(tdfs: list) -> pl.DataFrame:
+    """
+    Combine a list of polars dataframe into one polars dataframe
+
+    Args:
+        tdfs (list): a list of polars dataframe
+
+    Returns:
+        pl.DataFrame: a polars dataframe contains twitter_id, author_id, location
+    """
+    tdf = pl.concat(tdfs)
+
+    return tdf
+
+def count_number_of_tweets_by_author(tdf: pl.DataFrame) -> pl.DataFrame:
+    """
+    Count the number of tweets by each author
+
+    Args:
+        tdf (pl.DataFrame): a polars dataframe contains twitter_id, author_id, location
+
+    Returns:
+        pl.DataFrame: a polars dataframe contains author_id and number of tweets
+    """
+    # count the number of tweets by each author
+    author_tweet_count = tdf.groupby('author_id').agg(pl.count('tweet_id').alias('tweet_count')).sort('tweet_count', reverse=True)
+
+    return author_tweet_count
+
+def calculate_rank(tdf: pl.DataFrame, method: str) -> pl.DataFrame:
+    """
+    
+    """
+    return tdf.with_columns(pl.col('tweet_count').rank(method=method, descending=True).alias('rank'))
+
+def return_author_with_most_tweets(tdf: pl.DataFrame, top: int, save: bool, path: Path) -> pl.DataFrame:
+    """
+    Return the author with the most tweets
+    """
+    tdf = tdf.sort('rank', 'tweet_count', 'author_id', descending=[False, False, False]).filter(pl.col('rank')<= top)
+    if save:
+        tdf = tdf.select('rank', 'author_id', 'tweet_count')
+        tdf = tdf.with_columns(pl.col('rank').apply(lambda x: '#' + str(x)))
+        tdf.columns = ['Rank', 'Author Id', 'Number of Tweets Made']
+        tdf.write_csv(path / "data/result/task1.csv")
+        return None
+    else:
+        return tdf
+
+def count_number_of_tweets_by_gcc(tdf: pl.DataFrame) -> pl.DataFrame:
+    """
+    Count the number of tweets by each gcc
+    """
+    
+    tdf = tdf.select('gcc', 'tweet_id')\
+                .filter(~pl.col('gcc').is_null())\
+                .filter(~pl.col('gcc').str.contains(r"\dr[a-z]{3}"))\
+                .groupby('gcc').count()
+    
+    return tdf
+
+def return_gcc_with_tweets_count(tdf: pl.DataFrame, save: bool, path: Path) -> pl.DataFrame:
+    """
+    Count the number of tweets by each gcc not include rural area
+    Args:
+        tdf (pl.DataFrame): a polars dataframe contains gcc and number of tweets
+        save (bool): whether to save the result to csv file
+        path (Path): the path to save the result
+    Return:
+        pl.DataFrame: a polars dataframe contains gcc and number of tweets
+    """
+    tdf = tdf.sort('gcc', descending=False)
+    tdf.columns = ['Greater Captical City', 'Number of Tweets Made']
+    if save:
+        tdf.write_csv(path / "data/result/task2.csv")
+        return
+    else:
+        return tdf
