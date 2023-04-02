72a3f7358cd5a0f9541e18b1284808e6ccced7ce
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2023-04-02 12:18:59 +1000

    completed job submission script

--- submit.sh
+++ submit.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 # check virtualenv folder exist
-# if not then create new folder
 username=$(whoami)
 virtual_env="/home/${username}/virtualenv/python3.7.4"
 
-if [ ! -d "$virtual_env"]
+if [ ! -d "$virtual_env" ]; then
     module load python/3.7.4
     virtualenv "${virtual_env}"    
 fi
 
-echo "submitting job..."
-# submit the job
+echo "Submitting job... submit by ${username}"
 # sbatch slurm/1node1core.bigTwitter.slurm
 # sbatch slurm/1node8core.bigTwitter.slurm
 # sbatch slurm/2node8core.bigTwitter.slurm
 
-echo "job submitted"
+echo "Job submitted, print job status ::"
 
 # log current job list
 squeue -u $(whoami)
