2c765c3be4401303918c7b5deb8a2b4f04476e4c
rNLKJA <rNLKJA@non-use.com>
2023-04-02 12:17:55 +1000

    updated

--- submit.sh
+++ submit.sh
@@ -1,31 +1,16 @@
-#/bin/bash
+#!/bin/bash
 
 # check virtualenv folder exist
 # if not then create new folder
 username=$(whoami)
-folder_name="/home/${username}/virtualenv"
-target_virtual_env="/home/${username}/virtualenv/python3.7.4"
-virtual_env="python3.7.4"
-submission_pwd=$(pwd)
+virtual_env="/home/${username}/virtualenv/python3.7.4"
 
-echo $submission_pwd
-
-if [ ! -d "$folder_name" ]; then
-    mkdir "$folder_name"
-    echo "Create virtualenv folder"
-    
+if [ ! -d "$virtual_env"]
     module load python/3.7.4
-    virtualenv "${virtual_env}"
-
-else
-    if [ ! -d "$target_virtual_env" ]; then
-        module load python/3.7.4
-        virtualenv "${virtual_env}"
-    fi
+    virtualenv "${virtual_env}"    
 fi
 
 echo "submitting job..."
-cd ${submission_pwd}
 # submit the job
 # sbatch slurm/1node1core.bigTwitter.slurm
 # sbatch slurm/1node8core.bigTwitter.slurm
